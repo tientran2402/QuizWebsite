7031debe636c0c2b4ff98ad2e56ed2f426bd406a
Tran Tien <tientran163255@gmail.com>
2024-03-28 08:55:01 +0700

    QuizWebsite

---
+++ question.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question:"Loài động vật nào dưới đây thuộc lớp Thú?",
+        answer:"B. Cá heo",
+        options: [
+            "A. Cá mập",
+            "B. Cá heo",
+            "C. Cá chim",
+            "D. Cá chuồn"
+        ]
+    },
+    {
+        numb: 2,
+        question:"Đặc điểm cơ thể chia 3 phần, cơ thể phân đốt, đối xứng hai bên, bộ xương ngoài bằng chitin là của nhóm ngành nào?",
+        answer:"A. Chân khớp",
+        options: [
+            "A. Chân khớp",
+            "B. Giun đốt",
+            "C. Lưỡng cư",
+            "D. Cá"
+        ]
+    },
+    {
+        numb: 3,
+        question:"Động vật không xương sống bao gồm?",
+        answer:"B. Ruột khoang, giun, thân mềm, chân khớp",
+        options: [
+            "A. Cá, lưỡng cư, bò sát, chim, thú",
+            "B. Ruột khoang, giun, thân mềm, chân khớp",
+            "C. Bò sát, lưỡng cư, chân khớp, ruột khoang",
+            "D. Thú, chim, ruột khoang, cá, giun"
+        ]
+    },
+    {
+        numb: 4,
+        question:"Ngành động vật nào dưới đây có số lượng lớn nhất trong giới động vật?",
+        answer:"B. Chân khớp",
+        options: [
+            "A. Ruột khoang",
+            "B. Chân khớp",
+            "C. Lưỡng cư",
+            "D. Bò sát"
+        ]
+    },
+    {
+        numb: 5,
+        question:"Nhóm ngành nào của giới động vật có tổ chức cơ thể cao nhất?",
+        answer:"A. Thú",
+        options: [
+            "A. Thú",                 
+            "B. Chim",               
+            "C. Bò sát",            
+            "D. Cá"
+        ]
+    }
+];
